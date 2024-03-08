3018b107ea3dd6c7980e40e24f95e8741d3e456c
Chris St. John <csjcode@gmail.com>
2024-03-08 07:57:44 -0700

    Add react-icons and NavBar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,33 @@
+"use client";
+import Image from "next/image";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+
+const DynamicThemeSwitcher = dynamic(() => import("@/components/ThemeToggle"), {
+  ssr: false,
+});
+export default function Home() {
+  return (
+    <>
+      <nav className="navbar w-full px-4 py-3 shadow-lg">
+        <div className="flex items-center w-full">
+          <section className="font-bold mr-auto text-xl">
+            My Website Name
+          </section>
+          <ul className="nav-links flex  gap-8 items-center">
+            <Link href="/">
+              <li>Home</li>
+            </Link>
+            <Link href="/about">
+              <li>About</li>
+            </Link>
+            <Link href="/contacts">
+              <li>Contacts</li>
+            </Link>
+          </ul>
+          <DynamicThemeSwitcher />
+        </div>
+      </nav>
+    </>
+  );
+}
---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { BsFillMoonStarsFill, BsSun } from "react-icons/bs";
+
+function ThemeSwitcher() {
+  const storedTheme = window.localStorage.getItem("myTheme");
+
+  const checkTheme = () => {
+    if (storedTheme === "darkTheme") {
+      return false;
+    }
+    return true;
+  };
+
+  const [isLight, setIsLight] = useState(checkTheme);
+
+  function setLightTheme() {
+    setIsLight(true);
+    window.localStorage.setItem("myTheme", "lightTheme");
+  }
+
+  function setDarkTheme() {
+    setIsLight(false);
+    window.localStorage.setItem("myTheme", "darkTheme");
+  }
+
+  useEffect(() => {
+    const setTheme = () => {
+      const root = window.document.documentElement;
+      const operatingSystemThemeDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      );
+
+      if (storedTheme === "darkTheme" && operatingSystemThemeDark.matches) {
+        root.classList.add("dark");
+      }
+
+      if (storedTheme === "darkTheme") {
+        root.classList.add("dark");
+      }
+
+      if (storedTheme === "lightTheme") {
+        root.classList.remove("dark");
+      }
+    };
+    setTheme();
+
+    // console.log(`${storedTheme} selected`);
+  }, [storedTheme]);
+
+  return (
+    <div className="theme-switcher flex items-center ml-12">
+      <button
+        type="button"
+        className={`dark-mode-switch cursor-pointer mr-6 w-[40px] h-[40px] p-[10px] rounded-[100%]
+        border ${!isLight && "hidden"}`}
+        onClick={setDarkTheme}
+      >
+        <BsFillMoonStarsFill size={18} />
+      </button>
+      <button
+        type="button"
+        className={`light-mode-switch cursor-pointer mr-6 w-[40px] h-[40px] p-[10px] rounded-[100%]
+        border ${isLight && "hidden"}`}
+        onClick={setLightTheme}
+      >
+        <BsSun size={18} />
+      </button>
+    </div>
+  );
+}
+
+export default ThemeSwitcher;
