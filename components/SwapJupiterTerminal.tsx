b8748e79c435025bfea86755987d530b44bf86db
Chris St. John <csjcode@gmail.com>
2024-03-28 13:15:38 -0600

    Add SwapJupiterTerminal component and global type declaration

---
+++ components/SwapJupiterTerminal.tsx
@@ -0,0 +1,59 @@
+"use client"
+import { cn } from "@/utils/cn";
+import { Metadata } from "next";
+import Script from "next/script";
+import React from "react";
+
+type SwapJupiterTerminalProps = {};
+// export const metadata: Metadata = {
+//   title: "Swap Jupiter Terminal - Solana app",
+//   description: "Swap Jupiter Terminal Demo for my Solana app",
+// };
+const SwapJupiterTerminal = (props: SwapJupiterTerminalProps) => {
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  const [loadTimestamp, setLoadTimestamp] = React.useState(0);
+  const rpcUrl = process.env.NEXT_PUBLIC_MAIN_RPC_URL
+
+  if (loadTimestamp === 0) {
+    setLoadTimestamp(Date.now());
+  }
+
+  return (
+    <div>
+      <Script
+        src="https://terminal.jup.ag/main-v2.js"
+        onReady={() => {
+          window.Jupiter.init({
+            displayMode: "integrated",
+            integratedTargetId: "integrated-terminal",
+            endpoint: rpcUrl,
+            // passThroughWallet: walletContextState.wallet,
+            // onSuccess: ({ txid }: { txid: string }) => {
+            //   capture("user_swap", {
+            //     txn: txid,
+            //   });
+            // },
+            // formProps: {
+            //   initialInputMint: initialMint ? initialMint.toBase58() : undefined,
+            // },
+          });
+          const currentTime = Date.now();
+          const timeElapsed = currentTime - loadTimestamp;
+          const delay = Math.max(0, 1000 - timeElapsed);
+          setTimeout(() => {
+            setIsLoaded(true);
+          }, delay);
+        }}
+      />
+        <div className="h-full flex flex-col justify-start items-center content-start gap-8 w-4/5">
+        {/* {!isLoaded && <Loader label="Loading Jupiter swap..." className="mt-8" />} */}
+        <div
+          className={cn("max-w-[420px] px-3 transition-opacity", !isLoaded && "opacity-0")}
+          id="integrated-terminal"
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default SwapJupiterTerminal;
